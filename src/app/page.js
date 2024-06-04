900153f984033056d9e16c4d2e957a94ddf9a03f
Naoki <gtvofromb@gmail.com>
2024-06-04 15:57:20 +0900

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,39 @@
+'use client'
+
 import React from 'react';
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
 
 const Button = dynamic(() => import('@/components/Button'), {
   ssr: false
 });
 
+const PopUp = (props) => {
+  return (
+    <div>
+      <p>hello world</p>
+      <Button text="閉じる" handleClick={props.handleClick}/>
+    </div>
+  )
+}
+
 const HomePage = () => {
+
+  const [isVisible, setVisible] = useState(false)
+
+  const settingButtonClicked = () => {
+    setVisible(!isVisible)
+  }
+
   return (
     <div>
       <Button text="start" />
       <Button text="new game" />
-      <Button text="setting" />
+      <Button text="setting" handleClick={settingButtonClicked}/>
+      {isVisible && (
+        <PopUp handleClick={settingButtonClicked}/>
+      )}
     </div>
   );
 };
