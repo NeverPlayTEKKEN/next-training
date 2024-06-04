6ac1a2106ee17073a333440d9eae7cda6f141f54
Naoki <gtvofromb@gmail.com>
2024-06-04 16:19:46 +0900

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation'
 import dynamic from 'next/dynamic';
 import { useState } from 'react';
 
@@ -9,26 +9,27 @@ const Button = dynamic(() => import('@/components/Button'), {
   ssr: false
 });
 
-const PopUp = (props) => {
-  return (
-    <div>
-      <p>hello world</p>
-      <Button text="閉じる" handleClick={props.handleClick}/>
-    </div>
-  )
-}
+const PopUp = dynamic(() => import('@/components/PopUp'), {
+  ssr: false
+});
+
 
 const HomePage = () => {
 
   const [isVisible, setVisible] = useState(false)
+  const router = useRouter();
 
   const settingButtonClicked = () => {
     setVisible(!isVisible)
   }
 
+  const startButtonClicked = () => {
+    router.push('/about')
+  }
+
   return (
     <div>
-      <Button text="start" />
+      <Button text="start" handleClick={startButtonClicked}/>
       <Button text="new game" />
       <Button text="setting" handleClick={settingButtonClicked}/>
       {isVisible && (
---
+++ src/components/PopUp.js
@@ -0,0 +1,26 @@
+import dynamic from 'next/dynamic';
+
+const Button = dynamic(() => import('@/components/Button'), {
+    ssr: false
+  });
+
+const PopUp = (props) => {
+    const styles = {
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        backgroundColor: "#fff",
+        padding: "20px",
+        boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)"        
+    }
+
+    return (
+      <div style={styles}>
+        <p>hello world</p>
+        <Button text="閉じる" handleClick={props.handleClick}/>
+      </div>
+    )
+  }
+
+  export default PopUp
